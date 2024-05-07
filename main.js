d02c75b5953426e60800735ba35882a0c8d7a54c
karroumi <anasskarroumi2016@gmail.com>
2024-05-07 14:22:09 +0200

    add variables in js

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-let btn = document.getElementById("btn");
+let saturate = document.getElementById("saturate");
+let contrast = document.getElementById("contrast");
+let brightness = document.getElementById("brightness");
+let sepia = document.getElementById("sepia");
+let grayscale = document.getElementById("grayscale");
+let blur = document.getElementById("blur");
+let huerotate = document.getElementById("hue-rotate");
 
-btn.onclick = function () {
-  btn.style.background = "red";
-  btn.style.color = "yellow";
-};
+let upload = document.getElementById("upload");
+let download = document.getElementById("download");
+let img = document.getElementById("img");
+let reset = document.querySelector('span');
+let imgBox = document.querySelector('.img-box')
