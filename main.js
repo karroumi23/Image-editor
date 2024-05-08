62317b29d361b1d65fea46308b13a59c1a39a6b4
karroumi <anasskarroumi2016@gmail.com>
2024-05-08 14:58:10 +0200

    Reset default value for filters

--- main.js
+++ main.js
@@ -9,34 +9,45 @@ let huerotate = document.getElementById("hue-rotate");
 let upload = document.getElementById("upload");
 let download = document.getElementById("download");
 let img = document.getElementById("img");
-let reset = document.querySelector('span');
-let imgBox = document.querySelector('.img-box')
+let reset = document.querySelector("span");
+let imgBox = document.querySelector(".img-box");
 
+// Reset default value for filters
+function resetValue() {
+  img.style.filter = "none";
+  saturate.value = "100";
+  contrast.value = "100";
+  brightness.value = "100";
+  sepia.value = "0";
+  grayscale.value = "0";
+  blur.value = "0";
+  huerotate.value = "0";
 
-// onload function 
-window.onload = function (){
+}
+// onload function
+window.onload = function () {
   download.style.display = "none";
   reset.style.display = "none";
-  imgBox.style.display = "none"
-}
+  imgBox.style.display = "none";
+};
 
-
-// upload function 
-upload.onchange = function(){
+// upload function
+upload.onchange = function () {
+  resetValue();
   download.style.display = "block";
   reset.style.display = "block";
   imgBox.style.display = "block";
   let file = new FileReader();
   file.readAsDataURL(upload.files[0]);
-  file.onload = function(){
-    img.src = file.result
+  file.onload = function () {
+    img.src = file.result;
     // ??????????????????????
-  }
-}
-// filters function 
+  };
+};
+// filters function
 let filters = document.querySelectorAll("ul li input");
-filters.forEach(filter =>{
-   filter.addEventListener('input',function(){
+filters.forEach((filter) => {
+  filter.addEventListener("input", function () {
     img.style.filter = `
     saturate(${saturate.value}%)
     contrast(${contrast.value}%)
@@ -45,7 +56,6 @@ filters.forEach(filter =>{
     grayscale(${grayscale.value})
     blur(${blur.value}px)
     hue-rotate(${huerotate.value}deg)
-    `
-   })
-})
-
+    `;
+  });
+});
